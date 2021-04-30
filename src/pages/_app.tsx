ba3111972fd825f34919555ce3d1dc0c102aa714
Yagho Mattos <mattosyagho@gmail.com>
2021-04-30 17:11:00 -0300

    player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.scss'
 import styles from '../styles/app.module.scss';
 
 import { Header } from '../components/Header';
+import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }) {
   return(
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} />
       </main>
+      <Player />
     </div>
   )
 }
