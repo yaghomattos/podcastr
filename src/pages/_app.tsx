cfca37e9fdb57533a27f24dc8ac6cf1a59763ff2
Yagho Mattos <mattosyagho@gmail.com>
2021-04-30 16:56:02 -0300

    final header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/global.scss'
+import styles from '../styles/app.module.scss';
+
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
 
 export default MyApp
