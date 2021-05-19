168c7878a64a02801393c2a1c6fc9b56440242cd
Yagho Mattos <mattosyagho@gmail.com>
2021-05-19 16:36:02 -0300

    latest and all episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@ import { GetStaticProps } from 'next'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api'
-
 import { convertDurationToTimeString} from '../utils/convertDurationToTimeString'
+
+import styles from './home.module.scss'
 
 type Episode = {
   id: string;
@@ -18,14 +19,39 @@ type Episode = {
 }
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
-export default function Home(props) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+     <section className={styles.latestEpisodes}>
+       <h2>Últimos lançamentos</h2>
+
+      <ul>
+        {latestEpisodes.map(episode => {
+          return (
+            <li key={episode.id}>
+              <img src={episode.thumbnail} alt={episode.title} />
+
+              <div className={styles.episodeDetails}>
+                <a href="">{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>  
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio" />
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+     </section>
+
+     <section className={styles.allEpisodes}></section>
     </div>
   )
 }
@@ -53,9 +79,13 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.lenght)
+
   return {
     props: {
-      episodes: episodes,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   }
