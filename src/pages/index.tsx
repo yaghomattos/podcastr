cfe62eaf320fd9458f0e3bc298a778d5c4cac660
Yagho Mattos <mattosyagho@gmail.com>
2021-05-19 17:13:22 -0300

    style latest episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next'
+import Image from 'next/image'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api'
@@ -33,7 +34,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
         {latestEpisodes.map(episode => {
           return (
             <li key={episode.id}>
-              <img src={episode.thumbnail} alt={episode.title} />
+              <Image width={192} height={192} src={episode.thumbnail} alt={episode.title} objectFit="cover" />
 
               <div className={styles.episodeDetails}>
                 <a href="">{episode.title}</a>
